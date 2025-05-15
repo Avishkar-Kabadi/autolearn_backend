4f9a8b86e20f40634f6ab6371ad55780f7892b83
Avishkar Kabadi <155943160+Avishkar-Kabadi@users.noreply.github.com>
2025-05-15 19:42:25 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -4,11 +4,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+
 
 class RegisterView(APIView):
     @swagger_auto_schema(request_body=RegisterSerializer)
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
+        permission_classes = [AllowAny]
         if serializer.is_valid():
             serializer.save()
             return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
